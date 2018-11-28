948788cab02ff58614108d5c0740e0fef4c6b610
Zihan Zheng <zzh1996@mail.ustc.edu.cn>
2018-11-28 22:51:01 +0800

    merge commands, set name, fix gdb issue

--- ubuntu1804.sh
+++ ubuntu1804.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
-docker build -t ctf_ubuntu_1804 - <<DOCKERFILE_EOF || exit 1
+docker build -t zzh1996/ctf_ubuntu_1804 - <<DOCKERFILE_EOF || exit 1
 from ubuntu:18.04
 run rm /etc/dpkg/dpkg.cfg.d/excludes
 run sed -i 's/archive.ubuntu.com/mirrors.tuna.tsinghua.edu.cn/g' /etc/apt/sources.list
 #run mkdir -p ~/.config/pip
 #run printf "[global]\nindex-url = https://mirrors.ustc.edu.cn/pypi/web/simple\nformat = columns\n" > ~/.config/pip/pip.conf
 
-run dpkg --add-architecture i386
-run apt update && apt full-upgrade -y
+run dpkg --add-architecture i386 && apt update && apt full-upgrade -y && apt clean
 
-run apt install -y locales
-run sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && \
+run apt install -y locales && apt clean && \
+    sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && \
     dpkg-reconfigure --frontend=noninteractive locales && \
     update-locale LANG=en_US.UTF-8
 env LC_ALL en_US.UTF-8
@@ -25,33 +24,31 @@ run apt install -y git sudo bash make nano vim zsh tmux \
     manpages-posix manpages-posix-dev \
     libgmp3-dev libmpfr-dev libmpc-dev \
     nmap libssl-dev \
-    inetutils-ping dnsutils whois mtr
+    inetutils-ping dnsutils whois mtr \
+    && apt clean
 
-run pip3 install -U pip
-run pip2 install -U pip
-run pip3 install -U ipython pycrypto gmpy2 gmpy angr formatstring
-run pip3 install -U git+https://github.com/arthaud/python3-pwntools.git
-run pip2 install -U ipython pycrypto gmpy2 gmpy angr pwntools ropgadget
+run pip3 install -U pip && \
+    pip2 install -U pip
+run pip3 install -U ipython pycrypto gmpy2 gmpy angr formatstring && \
+    pip3 install -U git+https://github.com/arthaud/python3-pwntools.git && \
+    pip2 install -U ipython pycrypto gmpy2 gmpy angr pwntools ropgadget
 
-run useradd -ms /usr/bin/zsh ctf
-run adduser ctf sudo
-run echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+run useradd -ms /usr/bin/zsh ctf && \
+    adduser ctf sudo && \
+    echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 user ctf
 workdir /home/ctf
 
-run git clone https://github.com/scwuaptx/peda.git ~/.peda
-run cp ~/.peda/.inputrc ~/
-run git clone https://github.com/scwuaptx/Pwngdb.git ~/.Pwngdb
-run cp ~/.Pwngdb/.gdbinit ~/
+run git clone https://github.com/scwuaptx/peda.git ~/peda && cp ~/peda/.inputrc ~/ && \
+    git clone https://github.com/scwuaptx/Pwngdb.git ~/Pwngdb && cp ~/Pwngdb/.gdbinit ~/
 
-run sh -c "\$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" || true
-run curl https://raw.githubusercontent.com/zzh1996/zshrc/master/zshrc.sh > ~/.zshrc.sh
-run sed -i '/source \\\$ZSH\/oh-my-zsh.sh/isource ~/.zshrc.sh' ~/.zshrc
-run git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-run git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
-run git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-
-run curl https://raw.githubusercontent.com/wklken/vim-for-server/master/vimrc > ~/.vimrc
+run sh -c "\$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" || true && \
+    curl https://raw.githubusercontent.com/zzh1996/zshrc/master/zshrc.sh > ~/.zshrc.sh && \
+    sed -i '/source \\\$ZSH\/oh-my-zsh.sh/isource ~/.zshrc.sh' ~/.zshrc && \
+    git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions && \
+    git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions && \
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting && \
+    curl https://raw.githubusercontent.com/wklken/vim-for-server/master/vimrc > ~/.vimrc
 
 entrypoint zsh -i
 DOCKERFILE_EOF
@@ -60,4 +57,5 @@ docker run -it --rm --privileged --cap-add=SYS_PTRACE \
     --security-opt seccomp=unconfined \
     -v ${1:-`pwd`}:/root/ctf_docker \
     --hostname ctf_docker \
-    ctf_ubuntu_1804
+    --name ctf_ubuntu_1804 \
+    zzh1996/ctf_ubuntu_1804
